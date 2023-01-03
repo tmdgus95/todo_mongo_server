f14ce9073671bd67ca86b2531e9a521e7298f2af
tmdgus95 <8571053@naver.com>
2023-01-03 12:37:43 +0900

    기본 기능 구현

--- index.js
+++ index.js
@@ -12,12 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
     mongoose
         .connect(config.mongoURI)
-        .then(() => {
-            console.log("성공");
-        })
-        .catch((err) => {
-            console.log(`db 연결 실패${err}`);
-        });
+        .then(() => console.log("성공"))
+        .catch((err) => console.log(`db 연결 실패${err}`));
 
     console.log(`Example app listening on port ${port}`);
 });
@@ -25,6 +21,7 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
 app.get("/home", (req, res) => {
     res.send("왔군요");
 });
@@ -34,22 +31,53 @@ app.post("/api/post/submit", (req, res) => {
     const todoPost = new Todo(temp);
     todoPost
         .save()
-        .then(() => {
-            res.status(200).json({ success: true });
-        })
-        .catch((error) => {
-            res.status(400).json({ success: false });
-        });
+        .then(() => res.status(200).json({ success: true }))
+        .catch((error) => res.status(400).json({ success: false }));
 });
 
 app.post("/api/post/list", (req, res) => {
-    console.log("전체목록 호출");
     Todo.find({})
         .exec()
-        .then((doc) => {
-            res.status(200).json({ success: true, initTodo: doc });
-        })
-        .catch((error) => {
-            res.status(400).json({ success: false });
-        });
+        .then((doc) => res.status(200).json({ success: true, initTodo: doc }))
+        .catch((error) => res.status(400).json({ success: false }));
+});
+
+app.post("/api/post/updatetoggle", (req, res) => {
+    let temp = {
+        completed: req.body.completed,
+    };
+
+    Todo.updateOne({ id: req.body.id }, { $set: temp })
+        .exec()
+        .then((doc) => res.status(200).json({ success: true }))
+        .catch((error) => res.status(400).json({ success: false }));
+});
+
+app.post("/api/post/updatetitle", (req, res) => {
+    let temp = {
+        title: req.body.title,
+    };
+
+    Todo.updateOne({ id: req.body.id }, { $set: temp })
+        .exec()
+        .then((doc) => res.status(200).json({ success: true }))
+        .catch((error) => res.status(400).json({ success: false }));
+});
+
+app.post("/api/post/delete", (req, res) => {
+    let temp = {
+        id: req.body.id,
+    };
+
+    Todo.deleteOne({ $set: temp })
+        .exec()
+        .then((doc) => res.status(200).json({ success: true }))
+        .catch((error) => res.status(400).json({ success: false }));
+});
+
+app.post("/api/post/alldelete", (req, res) => {
+    Todo.deleteMany()
+        .exec()
+        .then((doc) => res.status(200).json({ success: true }))
+        .catch((error) => res.status(400).json({ success: false }));
 });
